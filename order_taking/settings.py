e85ca92759fd52cc6fb55466312db75719342655
Sunny <stfancyshirt@gmail.com>
2026-02-28 21:11:15 +0700

    'registration-is-done'

--- order_taking/settings.py
+++ order_taking/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# django_project/settings.py
+LOGIN_REDIRECT_URL = "home:home"
+LOGOUT_REDIRECT_URL = "home:home"
